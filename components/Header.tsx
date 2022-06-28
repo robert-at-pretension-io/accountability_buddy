7f0435a1f1529c80a2506a552e718897db0b4b0f
Robert Elliot Pahel-Short <robert@pretension.io>
2022-06-28 17:52:44 +0000

    added next

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+// Header react component using Next.js and Typescript
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'next/app';
+
+// Import Auth0Provider
+import { Auth0Provider } from '@auth0/auth0-react';
+
